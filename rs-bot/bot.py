b82f6f271db0ff14172d0f09664c8090fe084b49
boying44 <bt1124@nyu.edu>
2018-05-08 09:37:55 -0400

    Add requiremnents.txt

--- rs-bot/bot.py
+++ rs-bot/bot.py
@@ -1,16 +1,6 @@
-import numpy as np
 import pyautogui
-import cv2
 
 screenWidth, screenHeight = pyautogui.size()
 print(screenWidth, screenHeight)
 
 pyautogui.moveTo(screenWidth/2, screenHeight/2)
-image = pyautogui.screenshot()
-image = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
-# cv2.imwrite("in_memory_to_disk.png", image)
-cv2.imshow("image", image)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-
-# locateOnScreen('item.png')
--- web-server/README.md
+++ web-server/README.md
@@ -2,6 +2,7 @@ Web server built on Python sockets
 
 Jk trying to build a Flask clone now
 
+- [ ] Process headers into dictionaries
 - [ ] Multiple route decorators per function
 - [ ] Define status codes to messages
-- [ ]
+- [ ] render_template(files, ...data) Jinja2 template engine
