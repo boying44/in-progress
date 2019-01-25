fd57cff7c1c6abece974c63ca69ac2231ddaf3d0
boying44 <bt1124@nyu.edu>
2019-01-25 15:15:14 -0500

    Implemented recursive DFS

--- C++/btreedepth.cpp
+++ C++/btreedepth.cpp
@@ -1,1 +1,56 @@
 #include <iostream>
+#include <stack>
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+    // Recursive DFS, can we do iterative?
+    int maxDepth(TreeNode* root) {
+        stack<TreeNode*> s;
+        s.push(root);
+        int depth = 1;
+        int max = depth;
+        TreeNode* curr;
+
+        while(!s.empty()){
+            curr = s.top();
+            while(curr->left){
+                s.push(curr->left);
+                curr = curr->left;
+                ++depth;
+            }
+            if(depth > max){
+                max = depth;
+            }
+            // only pop after we finish checking a right side
+            // s.pop()
+            // --depth;
+
+            if(curr->right){
+                s.push(curr->right);
+                ++depth;
+            }
+        }
+        return max;
+    }
+
+};
+
+// Recursive Solution
+// class Solution {
+// public:
+//     int maxDepth(TreeNode* root) {
+//         if(!root){
+//             return 0;
+//         }
+//         int left = maxDepth(root->left)+1;
+//         int right = maxDepth(root->right)+1;
+//         return left > right ? left : right;
+//     }
+// };
